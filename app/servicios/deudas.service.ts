ed43e1c84fb132404e736736b7693d0ca016ecfa
Anthony Solis <asolis7378@uta.edu.ec>
2026-01-29 22:57:25 -0500

    .

--- app/servicios/deudas.service.ts
+++ app/servicios/deudas.service.ts
@@ -5,7 +5,7 @@ export async function consultarDeudas(
     tipo: string
 ): Promise<RespuestaConsulta> {
 
-    const res = await fetch('http://192.168.69.53:3000/api/deudas', {
+    const res = await fetch('http://186.46.219.181:3000/api/deudas', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ identificacion, tipo }),
